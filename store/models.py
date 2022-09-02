57ae703dd29dcfe28cf065fc47baed726d0baf7b
ambroseotundo <ambroseotundo9@gmail.com>
2022-09-02 15:25:44 -0400

    add models for the store app

--- store/models.py
+++ store/models.py
@@ -1,6 +1,8 @@
-from statistics import mode
+from itertools import product
 from django.db import models
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -8,6 +10,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 class Customer(models.Model):
     # store the default value
@@ -44,3 +47,28 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    # protect our data to prevent deletion
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    # prevent negative values
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+class Adress(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+   
+    # Creating a foreign key relationship between the Address model and the Customer model.
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    
