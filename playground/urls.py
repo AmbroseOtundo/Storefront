c24e4fcf0b9a89e8ee9d2a3ab6ce2ca4194414a6
ambroseotundo <ambroseotundo9@gmail.com>
2022-09-06 10:06:48 -0400

    test the debugger with ORM

--- playground/urls.py
+++ playground/urls.py
@@ -1,8 +1,7 @@
-
 from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
-    path('__debug__/', include('debug_toolbar.urls')),
+    path('hello/', views.say_hello),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from store.models import Product
 
-
+def say_hello(request):
+    query_set = Product.objects.filter(title__icontains='coffee')
+    return render(request, 'hello.html', {'name': 'Ambrose', 'products':list(query_set)})
