ea5618baa4fb7d0cbb25c8ec6802e0d23a0aa58e
ambroseotundo <ambroseotundo9@gmail.com>
2022-09-07 08:42:01 -0400

    more notes on ORM

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.db import transaction
+from django.db.models import Q, F, Func, Value, ExpressionWrapper
+from django.db.models.aggregates import *
+from store.models import Order, OrderItem, Product,Customer
+from django.contrib.contenttypes.models import ContentType
 from store.models import Product
+from tags.models import TaggedItem
+from django.db import connection
+
+
 
 def say_hello(request):
-    query_set = Product.objects.filter(title__icontains='coffee')
-    return render(request, 'hello.html', {'name': 'Ambrose', 'products':list(query_set)})
+# executing raw SQL queries.
+# examples
+# we can use also the with to get stored procedures
+    # Calling a stored procedure.
+    with connection.cursor() as cursor:
+        cursor.callproc('get_customers', ['params'])
+    return render(request, 'hello.html', {'name': 'Ambrose', 'result':list(queryset)})
